93a7e112b00409dffd47241e16835439f7ffdbbf
Greg Vondra <greg.vondra@outlook.com>
2024-05-19 07:31:25 -0500

    add logging

--- Source/Authorize/Authorize/Program.cs
+++ Source/Authorize/Authorize/Program.cs
@@ -1,6 +1,11 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using BrassLoon.Extensions.Logging;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Authorize
 {
@@ -9,8 +14,30 @@ namespace Authorize
         public static void Main(string[] args)
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+            builder.Host.ConfigureContainer((ContainerBuilder builder) => builder.RegisterModule(new AuthorizeApiModule()));
 
             // Add services to the container.
+            builder.Services.Configure<Settings>(builder.Configuration);
+
+            builder.Services.AddLogging(b =>
+            {
+                b.ClearProviders();
+                b.AddConsole();
+                Settings settings = new Settings();
+                builder.Configuration.Bind(settings);
+                if (settings.LogDomainId.HasValue && !string.IsNullOrEmpty(settings.BrassLoonLogRpcBaseAddress) && settings.BrassLoonClientId.HasValue)
+                {
+                    b.AddBrassLoonLogger(c =>
+                    {
+                        c.LogApiBaseAddress = settings.BrassLoonLogRpcBaseAddress;
+                        c.LogDomainId = settings.LogDomainId.Value;
+                        c.LogClientId = settings.BrassLoonClientId.Value;
+                        c.LogClientSecret = settings.BrassLoonClientSecret;
+                    });
+                }
+            });
+
             builder.Services.AddControllersWithViews();
 
             WebApplication app = builder.Build();
---
+++ Source/Authorize/Authorize/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Authorize
+{
+    public class Settings
+    {
+        public string LoginClientId { get; set; }
+        public string LoginRedirectUrl { get; set; }
+        public Guid? LogDomainId { get; set; }
+        public Guid? BrassLoonClientId { get; set; }
+        public string BrassLoonClientSecret { get; set; }
+        public string BrassLoonLogRpcBaseAddress { get; set; }
+    }
+}
