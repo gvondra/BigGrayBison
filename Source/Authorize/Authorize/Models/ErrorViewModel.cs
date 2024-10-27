b184460ba991c7755e4f8744bdf7a773fd4c6485
Greg Vondra <greg.vondra@outlook.com>
2024-10-27 08:12:26 -0500

    upgrade package references

--- Source/Authorize/Authorize/Models/ErrorViewModel.cs
+++ Source/Authorize/Authorize/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Authorize.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
